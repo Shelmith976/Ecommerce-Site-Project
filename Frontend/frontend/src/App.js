95e6676af642fa9d06eaf3a0f973706cba4c329c
RachaelMuthoni <muthonirachael50@gmail.com>
2022-07-21 14:53:16 +0300

    Frontend Register Navbar

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import './App.css';
 import Nav from './components/Navbar/Nav.js'
 import Register from './pages/Register/Register.js'
 import Home from './pages/Home/Home.js'
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Routes>
-          <Route path='/register' element={<Register/>} />
-        </Routes>    
-      </div>
-    </Router>
-=======
 import React from 'react';
 import Login from './components/Login/Login ';
 import Products from './components/Products/products';
@@ -22,13 +10,19 @@ import {Provider} from 'react-redux'
 import store from './components/features/store'
 function App() {
   return (
-    <Provider store ={store}> 
-    <div className="App">
-      {/* <Login /> */}
-      <Products />
-    </div>
-    </Provider>
->>>>>>> 7e2be69a996faabfee01a0ab3e3e6132ddf36cf7
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path='/register' element={<Register/>} />
+        </Routes> 
+        <Provider store ={store}> 
+          {/* <Login /> */}
+          <Products />
+        </Provider>   
+      </div>
+    </Router>
+    
   );
 }
 
--- Frontend/frontend/src/index.js
+++ Frontend/frontend/src/index.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from './App.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -14,7 +6,6 @@ import reportWebVitals from './reportWebVitals'
 import  {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ProductDetail from './components/routes/productDetail'
 const root = ReactDOM.createRoot(document.getElementById('root'))
->>>>>>> 7e2be69a996faabfee01a0ab3e3e6132ddf36cf7
 root.render(
   <BrowserRouter>
   <React.StrictMode>
@@ -24,9 +15,6 @@ root.render(
 
     </Routes>
   </React.StrictMode>
-<<<<<<< HEAD
-);
-=======
   </BrowserRouter>
 
 )
@@ -35,4 +23,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
->>>>>>> 7e2be69a996faabfee01a0ab3e3e6132ddf36cf7
